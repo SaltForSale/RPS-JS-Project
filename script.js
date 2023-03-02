0f9bbcae7667c368d2b22ca5fd066f11bbaaf5fc
SaltForSale <elee823@gmail.com>
2023-03-02 11:55:39 -0500

    Commit 8

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const getUserChoice = (userInput) => {
-  userInput = userInput.toLowerCase();
+const getUserChoice = (userChoice) => {
   if (
-    userInput === "rock" ||
-    userInput === "scissors" ||
-    userInput === "paper"
+    (userChoice === "rock") |
+    (userChoice === "scissors") |
+    (userChoice === "paper")
   ) {
-    return userInput;
+    return userChoice;
   } else {
     console.log("Error, please type:rock, paper, or scissors.");
   }
@@ -25,46 +24,51 @@ const getComputerChoice = () => {
 
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    document.getElementById('winner').src = assets/error.png
+    document.getElementById("winner").src = 'error.png';
     return "This game is a tie!";
   }
   if (userChoice === "rock") {
     if (computerChoice === "paper") {
-      document.getElementById('winner').src = assets/paper.png
+      document.getElementById("winner").src = 'paper.png';
+      document.getElementById("compImg").src = 'paper.png';
       return "Sorry, computer won!";
     } else {
-      document.getElementById('winner').src = assets/rock.png
+      document.getElementById("winner").src = 'rock.png';
+      document.getElementById("winner").src = 'paper.png';
       return "Congratulations, you won!";
     }
   }
 
   if (userChoice === "paper") {
     if (computerChoice === "scissors") {
-      document.getElementById('winner').src = assets/scissor.png
+      document.getElementById("winner").src = 'scissors.png';
+      document.getElementById("compImg").src = 'scissors.png';
       return "Sorry, computer won!";
     } else {
-      document.getElementById('winner').src = assets/paper.png
+      document.getElementById("winner").src = 'paper.png';
+      document.getElementById("winner").src = 'rock.png';
       return "Congratulations, you won!";
     }
   }
 
   if (userChoice === "scissors") {
     if (computerChoice === "rock") {
-      document.getElementById('winner').src = assets/rock.png
+      document.getElementById("winner").src = 'rock.png';
+      document.getElementById("compImg").src = 'rock.png';
       return "Sorry, computer won!";
     } else {
-      document.getElementById('winner').src = assets/scissor.png
+      document.getElementById("winner").src = 'scissors.png';
+      document.getElementById("compImg").src = 'paper.png';
       return "Congratulations, you won!";
     }
   }
 };
 
-const playGame = () => {
-  const userChoice = getUserChoice("paper");
+const playGame = (choice) => {
+  const userChoice = getUserChoice(choice);
   const computerChoice = getComputerChoice();
   console.log("You threw: " + userChoice);
   console.log("The computer threw: " + computerChoice);
-
   console.log(determineWinner(userChoice, computerChoice));
 };
 
