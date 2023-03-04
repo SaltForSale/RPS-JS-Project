73ca9eb6ed15474fd563a07c20ca8981af005731
Salt <89614490+SaltForSale@users.noreply.github.com>
2023-03-03 21:08:52 -0500

    Commit 11

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let pWins = 0
-let cWins = 0
+let pWins = 0;
+let cWins = 0;
 
 const getUserChoice = (userChoice) => {
-  if (
-    (userChoice === "rock") |
-    (userChoice === "scissors") |
-    (userChoice === "paper")
-  ) {
+  if (userChoice === "rock") {
+    document.getElementById("rock").src = "rockActive.png";
+    document.getElementById("scissors").src = "scissors.png";
+    document.getElementById("paper").src = "paper.png";
+    return userChoice;
+  }
+
+  if (userChoice === "scissors") {
+    document.getElementById("scissors").src = "scissorsActive.png";
+    document.getElementById("rock").src = "rock.png";
+    document.getElementById("paper").src = "paper.png";
+    return userChoice;
+  }
+
+  if (userChoice === "paper") {
+    document.getElementById("paper").src = "paperActive.png";
+    document.getElementById("rock").src = "rock.png";
+    document.getElementById("scissors").src = "scissors.png";
     return userChoice;
   } else {
     console.log("Error, please type:rock, paper, or scissors.");
@@ -27,18 +40,18 @@ const getComputerChoice = () => {
 
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    document.getElementById("winner").src = 'error.png';
+    document.getElementById("winner").src = "error.png";
     return "This game is a tie!";
   }
   if (userChoice === "rock") {
     if (computerChoice === "paper") {
-      document.getElementById("winner").src = 'paper.png';
-      document.getElementById("compImg").src = 'paper.png';
+      document.getElementById("winner").src = "paper.png";
+      document.getElementById("compImg").src = "paper.png";
       document.getElementById("cWins").innerHTML = cWins++;
       return "Sorry, computer won!";
     } else {
-      document.getElementById("winner").src = 'rock.png';
-      document.getElementById("winner").src = 'paper.png';
+      document.getElementById("winner").src = "rock.png";
+      document.getElementById("winner").src = "paper.png";
       document.getElementById("pWins").innerHTML = pWins++;
       return "Congratulations, you won!";
     }
@@ -46,13 +59,13 @@ const determineWinner = (userChoice, computerChoice) => {
 
   if (userChoice === "paper") {
     if (computerChoice === "scissors") {
-      document.getElementById("winner").src = 'scissors.png';
-      document.getElementById("compImg").src = 'scissors.png';
+      document.getElementById("winner").src = "scissors.png";
+      document.getElementById("compImg").src = "scissors.png";
       document.getElementById("cWins").innerHTML = cWins++;
       return "Sorry, computer won!";
     } else {
-      document.getElementById("winner").src = 'paper.png';
-      document.getElementById("winner").src = 'rock.png';
+      document.getElementById("winner").src = "paper.png";
+      document.getElementById("winner").src = "rock.png";
       document.getElementById("pWins").innerHTML = pWins++;
       return "Congratulations, you won!";
     }
@@ -60,13 +73,13 @@ const determineWinner = (userChoice, computerChoice) => {
 
   if (userChoice === "scissors") {
     if (computerChoice === "rock") {
-      document.getElementById("winner").src = 'rock.png';
-      document.getElementById("compImg").src = 'rock.png';
+      document.getElementById("winner").src = "rock.png";
+      document.getElementById("compImg").src = "rock.png";
       document.getElementById("cWins").innerHTML = cWins++;
       return "Sorry, computer won!";
     } else {
-      document.getElementById("winner").src = 'scissors.png';
-      document.getElementById("compImg").src = 'paper.png';
+      document.getElementById("winner").src = "scissors.png";
+      document.getElementById("compImg").src = "paper.png";
       document.getElementById("pWins").innerHTML = pWins++;
       return "Congratulations, you won!";
     }
@@ -76,13 +89,12 @@ const determineWinner = (userChoice, computerChoice) => {
 const playGame = (choice) => {
   const userChoice = getUserChoice(choice);
   const computerChoice = getComputerChoice();
+  let ratio = (pWins / cWins).toFixed(2);
+  console.log(ratio);
+  document.getElementById("ratio").innerHTML = ratio;
   console.log("You threw: " + userChoice);
   console.log("The computer threw: " + computerChoice);
   console.log(determineWinner(userChoice, computerChoice));
 };
-
-function runRatio = (pWins, cWins) => {
-  console.log(pWins/cWins)
-}
 
 playGame();
